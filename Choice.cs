05ad1dc4a61ef5a0f6b408415faa9c459f73fd63
Venoflux <erolsarpk@gmail.com>
2021-12-16 18:58:08 +0100

    KartBulucu() and ListeBulucu() are added

--- Choice.cs
+++ Choice.cs
@@ -8,7 +8,7 @@ namespace proje2_ToDo
         static Dictionary<int, string> worker_dict = new Dictionary<int, string>(){
             {123, "Ahmet Baran"},
             {321, "Rutkay Kuzgun"},
-            {523, "Baran Siler"}
+            {523, "Esra Siler"}
         };
 
         public static void KartEkle()
@@ -44,6 +44,16 @@ namespace proje2_ToDo
         }
 
 
+        public static void KartUpdate()
+        {
+            Console.WriteLine("Güncellemek İstediğiniz Kart'ın Başlığı :");
+            string title = Console.ReadLine();
+            
+            
+            
+        }
+
+
 
         ///<summary>
         /// Kart template'i ekrana yazdırılır.
@@ -64,13 +74,45 @@ namespace proje2_ToDo
             foreach (int member in my_dict.Keys)
                 if (member == id)
                     return true;
-            
             return false;
         }
 
+        ///<summary>
+        /// İçinde Kart objesi olan bir listenin Title indexini verir.
+        ///</summary>
+        ///<returns>int index, bulamaz ise -1</returns>
+        private static int KartBulucu(string input, List<Kart> myList)
+        {
+            return myList.FindIndex(x => x.Title == input);
+        }
+
+        ///<summary>
+        /// Her Liste içini kontrol ederek aramanın hangi listede olduğunu verir.
+        ///</summary>
+        ///<returns>int 1-3, bulamaz ise -1</returns>
+        private static int ListeBulucu(string input)
+        {
+            int index = KartBulucu(input, todo);
+            if(index != -1)
+                return 1;
+            else{
+                    index = KartBulucu(input, progress);
+                    if(index != -1)
+                        return 2;
+                    else{
+                        index = KartBulucu(input, done);
+                        if(index != -1)
+                            return 3;
+                        else
+                            return -1;
+                    }
+                }
+        }
         private static void FailProtocol()
         {
-
+            
         }
+
+
     }
 }
