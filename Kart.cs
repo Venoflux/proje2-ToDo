7c57b7419be1debd6d12741c7dff1c261632f395
Venoflux <erolsarpk@gmail.com>
2021-12-27 16:51:06 +0300

    KartBilgileri added to print object attributes

--- Kart.cs
+++ Kart.cs
@@ -2,10 +2,10 @@ namespace proje2_ToDo
 {
     class Kart
     {
-        public string Title { get; set; }
-        public string Context { get; set; }
-        private int Id { get; }
-        private int Size { get; }
+        public string Title;
+        public string Context;
+        private int Id;
+        private int Size;
 
 
         public Kart(string title, string context, int id, int size)
@@ -14,6 +14,17 @@ namespace proje2_ToDo
             this.Context = context;
             this.Id = id;
             this.Size = size;
+        }
+
+        public void KartBilgileri(Dictionary<int, string> my_dict)
+        {
+            string name = my_dict.GetValueOrDefault(Id);
+            string size = Enum.GetName(typeof(SizeTypes), Size);
+            
+            Console.WriteLine($@"Başlık: {Title}
+İçerik: {Context}
+Atanan Kişi: {name}
+Büyükşük: {size}");
         }
     }
 
